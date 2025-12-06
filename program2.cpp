760d267b9979041ba3aae5b54d3b302ae5b9bd42
pratik.s.suryawanshi <pratik.s.suryawanshii@gmail.com>
2025-12-06 15:40:45 +0530

    Update program2.cpp

--- program2.cpp
+++ program2.cpp
@@ -1,13 +1,10 @@
-#include <iostream>     // Header file for input/output stream
+#include <iostream>
+using namespace std;
 
-using namespace std;    // Use the standard namespace
-
-// Main function - program entry point
 int main()
 {
-    // Print message to the console
     cout << "jay ganesh...\n";
-
-    // Return 0 to indicate successful execution
+    
     return 0;
 }
+
